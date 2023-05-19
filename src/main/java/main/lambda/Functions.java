17f861a0f4de9a514da3fa8ec12eb733b91a66f3
Varlenn <73470474+Varlenn@users.noreply.github.com>
2023-05-19 12:56:09 +0300

    remove unnecessary

--- src/main/java/main/lambda/Functions.java
+++ src/main/java/main/lambda/Functions.java
@@ -8,15 +8,6 @@ public class Functions {
         return strings;
     }
 
-    public static List<String> upCaseFirst(List<String> strings) {
-        strings.replaceAll(str -> {
-            final char[] chars = str.toCharArray();
-            chars[0] = Character.toUpperCase(chars[0]);
-            return String.valueOf(chars);
-        });
-        return strings;
-    }
-
     public static int sumSqrt(List<Integer> list) {
         return list.stream().mapToInt(value -> value * value).sum();
     }
